e95bb39142373bd195fa83d0167768cc6b9a7c97
Jeremy Sinitean <jeremy.sinitean@gmail.com>
2016-08-05 10:17:27 -0500

    Update nav + add Sakwa

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,5 +36,16 @@ function toggle() {
 		}
 }
 
-
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
 
